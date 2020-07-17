3e1d183b49c62451bedc5d0b6638edfae2b77c2f
Veronicaslater86 <karima.krachai@gmail.com>
2020-07-17 10:20:03 +0200

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,156 @@
-const URL = "http://localhost:8000/heroes"
+const URL = "http://localhost:8000/heroes";
+const formPostHero = document.getElementById("formPostHero");
+const searchbar = document.getElementById("searchbar");
+var heroes = [];
+const formOrder = document.getElementById("order")
+//Header fixed on scroll
 
-function getAllHeroes(){
-    axios
-    .get(URL+ "?_sort=id&_order=asc")
+window.addEventListener("scroll", () => {
+  let header = document.getElementById("header")
+  if (window.pageYOffset > 0) {
+      header.classList.add("header-active");
+  } else {
+      header.classList.remove("header-active")
+  }
+})
+
+// Function get all heroes
+
+function getAllHeroes() {
+  axios
+    .get(URL + "?_sort=id&_order=asc")
     .then((apiRes) => {
-        const heroes = apiRes.data
-        displayAllHeroes(heroes)
+      heroes = apiRes.data
+      displayAllHeroes(heroes)
     })
     .catch((apiErr) => console.error(apiErr))
 }
 
-function displayAllHeroes(list){
-const ul = document.getElementById("listHeroes");
-ul.innerHTML="";
-list.forEach((hero) => {
+// function display heroes
+
+function displayAllHeroes(list) {
+  const ul = document.getElementById("listHeroes");
+  ul.innerHTML = "";
+  list.forEach((hero) => {
     const li = document.createElement("li");
     li.classList.add("hero");
     li.setAttribute("data-hero-id", hero.id);
     // setup li's markup
     li.innerHTML = `
             <h2>${hero.name}</h2>
-            <figure><img src="${hero.image.url}" alt="${hero.name}"></figure>
+            <figure class="carte"><img src="${hero.image.url}" alt="${hero.name}"></figure>
             <div class="buttons">
                 <button class="btn remove">remove</button>
                 <button class="btn details">details</button>
             </div>
         `;
-
-
-    // remove
-    async function deleteOneUser(id) {
-      console.log(id);
-      try {
-        await axios.delete(`${URL}/${id}`);
-        removeUserFromDocument(id);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    
-    function removeUserFromDocument(idHero) {
-      // https://developer.mozilla.org/fr/docs/Web/CSS/S%C3%A9lecteurs_d_attribut
-      const cardToRemove = document.querySelector(`[data-user-id="${idHero}"]`);
-      cardToRemove.remove();
-    }
     // get the btn as an element object
     const btnDetails = li.querySelector(".btn.details");
     const btnRemove = li.querySelector(".btn.remove");
 
     // setup event listeners on the btn
     btnDetails.onclick = () => {
-      getOneUser(hero.id);
+      getOneHero(hero.id);
     };
 
     btnRemove.onclick = () => {
-      deleteOneUser(hero.id);
+      deleteOneHero(hero.id);
     };
 
     ul.appendChild(li);
   });
 }
 
+// Function post new hero
+
+async function postNewHeroe() {
+  const name = document.getElementById("name").value;
+  // const intelligence = document.getElementById("intelligence").value;
+
+
+  try {
+    await axios.post(URL, {
+      name,
+    });
+    console.log(name)
+    getAllHeroes();
+
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+
+function getOneHeroe(id) {
+  axios
+    .get(`${URL}/${id}`)
+    .then((apiRes) => {
+      const heroe = apiRes.data;
+      displayOneHeroe(heroe);
+    })
+    .catch((apiErr) => console.error(apiErr));
+}
+
+
+
+// Function remove
+
+async function deleteOneHero(id) {
+  console.log(id);
+  try {
+    await axios.delete(`${URL}/${id}`);
+    removeHeroFromDocument(id);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function removeHeroFromDocument(idHero) {
+  const cardToRemove = document.querySelector(`[data-user-id="${idHero}"]`);
+  cardToRemove.remove();
+
+}
+
+// Function filtre by name
+
+searchbar.addEventListener("keyup", (element) => {
+  const input = element.target.value;
+  const filteredHeroes = heroes.filter((hero) => {
+    return (
+      hero.name.toLowerCase().includes(input.toLowerCase())
+    )
+  })
+  displayAllHeroes(filteredHeroes)
+})
+
+// Function filtre by order
+function selectedorder() {
+  console.log(formOrder.value)
+  const order = formOrder.value
+
+  if (order === "desc") {
+    axios
+      .get(URL + "?_sort=id&_order=desc")
+      .then((apiRes) => {
+        heroes = apiRes.data
+        displayAllHeroes(heroes)
+      })
+      .catch((apiErr) => console.error(apiErr))
+    console.log("ohmygod")
+  }else{
+    axios
+      .get(URL + "?_sort=id&_order=asc")
+      .then((apiRes) => {
+        heroes = apiRes.data
+        displayAllHeroes(heroes)
+      })
+      .catch((apiErr) => console.error(apiErr))
+    console.log("good")
+  }
+
+}
+formOrder.onchange = selectedorder
+
 
 getAllHeroes()
+formPostHero.querySelector(".btn").onclick = postNewHeroe;
